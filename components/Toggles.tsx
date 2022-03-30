5a72d4d361f1d6744ba24acf3401f93b2271c7c7
lumenwrites <lumenwrites@gmail.com>
2022-03-30 02:09:54 +0100

    Toggles animated.

--- components/Toggles.tsx
+++ components/Toggles.tsx
@@ -1,22 +1,57 @@
-import { useState } from "react"
+import { useState } from 'react'
+import questions from 'data/questions.json'
 
-export default function Toggles({ toggles }) {
-  const [selectedOption, setSelectedOption] = useState(0)
+export default function Toggles() {
+  const [toggles, setToggles] = useState(questions[0].toggles)
   return (
     <div className="toggles">
-      {toggles.map((t) => (
-        <Toggle toggle={t} selectedOption={selectedOption} />
+      {toggles.map((toggle) => (
+        <Toggle
+          key={toggle.id}
+          toggle={toggle}
+          toggles={toggles}
+          setToggles={setToggles}
+        />
       ))}
     </div>
   )
 }
 
-function Toggle({ toggle, selectedOption }) {
+function Toggle({ toggle, toggles, setToggles }) {
+  const optionWidth = 100 / toggle.options.length
+  function selectOption(idx) {
+    setToggles((prev) => {
+      const updatedToggles = prev.map((t) => {
+        if (t.id === toggle.id) {
+          return { ...toggle, selectedOption: idx }
+        }
+        return t
+      })
+      return updatedToggles
+    })
+  }
   return (
     <div className="toggle">
-      {toggle.options.map((option, idx) => (
-        <div className={`option ${idx === selectedOption ? "active" : ""}`}>{option}</div>
-      ))}
+      <div className="options">
+        {toggle.options.map((option, idx) => (
+          <div
+            key={idx}
+            className={`option ${
+              idx === toggle.selectedOption ? 'active' : ''
+            }`}
+            onClick={() => selectOption(idx)}
+          >
+            {option}
+          </div>
+        ))}
+      </div>
+      <div
+        className="activeBG"
+        style={{
+          width: `${optionWidth}%`,
+          left: `${toggle.selectedOption * optionWidth}%`,
+        }}
+      />
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import Toggles from 'components/Toggles'
-import questions from 'data/questions.json'
 
 export default function Home() {
   return (
     <div className={'question'}>
-      <Toggles toggles={questions[0].toggles} />
+      <Toggles />
     </div>
   )
 }
