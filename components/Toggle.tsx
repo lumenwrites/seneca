f5102697bc4e50c86ad37527c49e2ca3c0b676d6
lumenwrites <lumenwrites@gmail.com>
2022-04-09 01:29:23 +0100

    Refactor toggle into a separate file.

---
+++ components/Toggle.tsx
@@ -0,0 +1,63 @@
+import { FC } from 'react'
+import { useTogglesContext } from './Question'
+
+interface ToggleProps {
+  toggle: {
+    id: number,
+    correctOption: number,
+    selectedOption: number,
+    options: string[]
+  }
+}
+
+export const Toggle:FC<ToggleProps> = ({ toggle }) => {
+  const { isLocked, setToggles } = useTogglesContext()
+  const optionSize = 100 / toggle.options.length
+
+  function selectOption(idx) {
+    if (isLocked) return
+    setToggles((prev) => {
+      const updatedToggles = prev.map((t) => {
+        if (t.id === toggle.id) {
+          return { ...toggle, selectedOption: idx }
+        }
+        return t
+      })
+      return updatedToggles
+    })
+  }
+
+  let activeBackgroundStyles = {
+    width: `${optionSize}%`,
+    left: `${toggle.selectedOption * optionSize}%`,
+  } as any
+  // Responsive styles
+  if (window.innerWidth < 700) {
+    activeBackgroundStyles = {
+      height: `${optionSize}%`,
+      top: `${toggle.selectedOption * optionSize}%`,
+    }
+  }
+  
+  return (
+    <div className="toggle">
+      <div className="options">
+        {toggle.options.map((option, idx) => {
+          let className = 'option'
+          if (idx === toggle.selectedOption) className += ' active'
+          if (isLocked) className += ' locked'
+          return (
+            <div
+              key={idx}
+              className={className}
+              onClick={() => selectOption(idx)}
+            >
+              {option}
+            </div>
+          )
+        })}
+      </div>
+      <div className="activeBG" style={activeBackgroundStyles} />
+    </div>
+  )
+}
--- components/Toggles.tsx
+++ components/Toggles.tsx
@@ -1,62 +1,13 @@
 import { useTogglesContext } from './Question'
+import { Toggle } from './Toggle'
 
 export default function Toggles() {
   const { toggles } = useTogglesContext()
-
   return (
     <div className={`toggles`}>
       {toggles.map((toggle) => (
         <Toggle key={toggle.id} toggle={toggle} />
       ))}
-    </div>
-  )
-}
-
-function Toggle({ toggle }) {
-  const { toggles, isLocked, setToggles } = useTogglesContext()
-  const optionSize = 100 / toggle.options.length
-  function selectOption(idx) {
-    if (isLocked) return
-    setToggles((prev) => {
-      const updatedToggles = prev.map((t) => {
-        if (t.id === toggle.id) {
-          return { ...toggle, selectedOption: idx }
-        }
-        return t
-      })
-      return updatedToggles
-    })
-  }
-  let activeBackgroundStyles = {
-    width: `${optionSize}%`,
-    left: `${toggle.selectedOption * optionSize}%`,
-  } as any
-  // Responsive
-  if (window.innerWidth < 700) {
-    activeBackgroundStyles = {
-      height: `${optionSize}%`,
-      top: `${toggle.selectedOption * optionSize}%`,
-    }
-  }
-  return (
-    <div className="toggle">
-      <div className="options">
-        {toggle.options.map((option, idx) => {
-          let className = 'option'
-          if (idx === toggle.selectedOption) className += ' active'
-          if (isLocked) className += ' locked'
-          return (
-            <div
-              key={idx}
-              className={className}
-              onClick={() => selectOption(idx)}
-            >
-              {option}
-            </div>
-          )
-        })}
-      </div>
-      <div className="activeBG" style={activeBackgroundStyles} />
     </div>
   )
 }
